3d8d465394a9ee9e59366fdfd84844938808f54b
kirsmndz <108765939+kirsmndz@users.noreply.github.com>
2022-07-06 16:28:10 +0800

    Add files via upload

---
+++ LBYEC2A Project/notesforcode.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+
+
+#define BUFFER_SIZE 100
+#define USERNAME "MKAM"
+#define PASSWORD "LBYEC2A"
+
+int main(){
+    char password[BUFFER_SIZE + 1];
+    char username[BUFFER_SIZE + 1];
+    char ch;
+    int characterPosition = 0;
+    printf("\nEnter your username: ");
+    scanf("%[^\n]%*c", username);
+
+    printf("Enter your password: ");
+
+    while (1) { /// while its true (missing: it has to end eventually) what is true ba? how is it supposed to end?
+        ch = getch();
+        if (ch==13) { // ASCII Code of ENTER: 13
+            break;
+        } else if (ch == 8) { // ASCII Code of BACKSPACE: 08
+            if(characterPosition > 0){
+                characterPosition--;
+                password[characterPosition] = '\0';
+                printf("\b \b");
+            }
+        } else if (ch == 32 || ch == 9) {// ASCII Code of SPACE: 32 | ASCII Code of TAB: 09
+            continue;
+        }else {
+        password[characterPosition] = ch; //copies number of characters to print asterisks
+        characterPosition++;
+        printf("*");
+        }
+    } /// loop is not ending
+
+    username[characterPosition] = '\0';
+    password[characterPosition] = '\0';
+    if ((strcmp(username, USERNAME) == 0) && (strcmp(password, PASSWORD) == 0)) {
+        puts("\n\nVALID!");
+
+        char name[BUFFER_SIZE + 1];
+        char degree[BUFFER_SIZE + 1];
+        char studentID[BUFFER_SIZE + 1]; 
+        char address[BUFFER_SIZE + 1];
+        char birthday[BUFFER_SIZE + 1];
+
+        printf("\nEnter your Name: ");
+        scanf("%[^\n]%*c", name);
+    
+        printf("Enter you Degree program: ");
+        scanf("%[^\n]%*c", degree);
+         
+        printf("Enter your student ID no.: ");
+        scanf("%[^\n]%*c", studentID);
+
+        printf("Enter your address: ");
+        scanf("%[^\n]%*c", address);
+  
+        printf("Enter your birthday: ");
+        scanf("%[^\n]%*c", birthday);
+
+        printf("\nHello %s!\n", name);
+
+        printf("\nThe following is your profile: \n");
+
+        printf("\nName: %s", name);
+        printf("\nDegree Program: %s", degree);
+        printf("\nStudent ID no.: %s", studentID);
+        printf("\nAddress: %s", address);
+        printf("\nBirthday: %s", birthday);
+
+    } else {
+        puts("\n\nINVALID!");
+    }
+    return 0;
+
+}
---
+++ LBYEC2A Project/program.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+
+#define SIZE 20
+#define USERNAME1 "MKAM"
+#define PASSWORD1 "LBYEC2A"
+#define USERNAME2 "cprogrammer"
+#define PASSWORD2 "alohamora"
+#define USERNAME3 "tentative1"
+#define PASSWORD3 "tentative2"
+
+int main()
+{
+    char username[SIZE], password[SIZE], USERNAME1[SIZE], PASSWORD1[SIZE], USERNAME2[SIZE], PASSWORD2[SIZE], USERNAME3[SIZE],PASSWORD3[SIZE];
+    char ch;
+
+    printf("Enter your username: ");
+    scanf("%[^\n]%*c", &username);
+    printf("Enter your password: ");
+    while 
+}
